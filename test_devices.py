cc7b04d117cc9ba10e3047e86270ea09b07a45da
Cyrille Toutain <toutain.cyrille@gmail.com>
2020-11-19 22:46:53 +0100

    modification boucle tests saisie user et couleur option menu en rouge

--- test_devices.py
+++ test_devices.py
@@ -8,13 +8,14 @@ from netmiko.ssh_exception import NetMikoTimeoutException
 from netmiko.ssh_exception import AuthenticationException
 import linecache
 
+# on effece l'ecran
 os.system("clear")
 
 ### initialisation des paramétres date du jour et ip du serveur de sauvegarde
 now = datetime.datetime.now()
 scp_server = '192.168.122.36'
 
-# Initialisation d
+# Initialisation format date
 hour=time.strftime('%H')
 minute=time.strftime('%M')
 day=time.strftime('%d')
@@ -22,6 +23,7 @@ month=time.strftime('%m')
 year=time.strftime('%Y')
 today=day+"-"+month+"-"+year+"-"+hour+minute
 
+# initialisation de l'affichage du menu de sauvegarde
 affichage_menu ="""
 \033[34m#############################
 #                           #
@@ -34,7 +36,7 @@ Choissisez une option
 \t\033[31m 3\033[0m - Envoi dans le Cloud
 \t\033[31m 4\033[0m - Quitter
 """
-
+# initialisation de l'affichage du menu cloud
 affiche_cloud = """
 \033[34m#############################
 #                           #
@@ -42,16 +44,20 @@ affiche_cloud = """
 #                           #
 #############################\033[0m 
 Choissisez une option 
-\t1 - Switch
-\t2 - Routeur
-\t3 - Retour
+\t\033[31m1\033[0m  - Switch
+\t\033[31m2\033[0m  - Routeur
+\t\033[31m3\033[0m  - Retour
 """
+
+# fonctions pour choisr le type de matériel à sauvegarder
 def lecture_devices(device_type):
 	device_type_init = device_type 							# recupération du type de matériel switch ou routeur
 	chemin_listes = (f'Devices/{device_type_init}_cisco')	# chemin du fichier de parametres
 	device_lecture = open(chemin_listes)					# lecture du fichier
 	compteur = 0											# initialisation du compteur
+	
 	print(f"\nVoici la liste des {device_type_init}s")
+	
 	for device in device_lecture.readlines():
 		infos = device.split('_')
 		hostname = infos[0] 		#récupération de l'adress IP
@@ -60,19 +66,28 @@ def lecture_devices(device_type):
 		
 		### affichage de infos pour chaque matériel
 		print (f'{compteur} : Nom : {hostname} - IP: {adress_ip}')
-		
-	choix_type_sauvegarde = input(f"\nEntrez votre choix de [1-{compteur}] ou [T] pour Tous : ")
-	device_lecture.close()			# on ferme le fichier
-	return choix_type_sauvegarde	# on retourne le choix de l'utilisateur
+	
+	choix_type_sauvegarde = int(input(f"\nEntrez votre choix de [\033[31m1-{compteur}\033[0m] ou [\033[31m0\033[0m] pour Tous : "))
+	while choix_type_sauvegarde > compteur :
+		try:
+			choix_type_sauvegarde = int(input(f"\nEntrez votre choix de [\033[31m1-{compteur}\033[0m] ou [\033[31m0\033[0m] pour Tous : "))
+			continue
+		except ValueError:
+			print("Oops! Ce n'est pas un nombre valide. Esaaye encore...")
 
+	device_lecture.close()			# on ferme le fichier
+	return choix_type_sauvegarde 	# on retourne le choix de l'utilisateur
+			
+# fonctions pour créer la sauvegarde par rapport au choix et type de materiel
 def sauvegarde_devices(user_choice, device_type):
 	device_type_init = device_type 										# recupération du type de matériel switch ou routeur						
 	chemin_listes = (f'Devices/{device_type_init}_cisco')				# chemin du fichier de parametres
 	device_lecture = open(chemin_listes)								# lecture du fichier
 	ligne_choice = user_choice											# récupéaration du choix de l'utilsiateur 	
-	scp_folder = (f'PROJET/P6/Sauvegarde/{device_type_init}')
+	scp_folder = (f'PROJET/P6/Sauvegarde/{device_type_init}')			# chemin du répertoire de sauvegarde
 	
-	if ligne_choice.isdigit() == True:	
+	# tests si choix est valide
+	if ligne_choice != 0 :	
 		device_visu = linecache.getline(chemin_listes, int(ligne_choice))	# récupération de la ligne choisit par l'utilisateur	
 		### récupération des donnés du fichier dans des variables
 		infos = device_visu.split('_')
@@ -100,13 +115,17 @@ def sauvegarde_devices(user_choice, device_type):
 			net_connect = ConnectHandler(**devices)
 		except NetMikoTimeoutException:
 			print ('Device {} is not reachable.'.format(hostname))
+			exit()
 		except AuthenticationException:
 			print ('Authentication Failure.')
+			exit()
 		except SSHException:
 			print ('Make sure SSH is enabled in device.')
+			exit()
 		
 		### initialisation de la connexion
 		print ('Initiating configuration backup to ' + hostname + '\n')
+		time.sleep(5)
 		net_connect.enable()
 
 		### creation de la commande pour la sauvagarde
@@ -120,7 +139,7 @@ def sauvegarde_devices(user_choice, device_type):
 				output += net_connect.send_command_timing(command_string="\n", strip_prompt=False, strip_command=False)
 		if "Destination username" in output:
 				output += net_connect.send_command_timing(command_string="\n", strip_prompt=False, strip_command=False)
-		time.sleep(10)	
+		#time.sleep(10)	
 		if "Destination filename" in output:
 				output += net_connect.send_command_timing(command_string="\n", strip_prompt=False, strip_command=False)
 		time.sleep(20)
@@ -128,12 +147,12 @@ def sauvegarde_devices(user_choice, device_type):
 		### Déconnexion
 		net_connect.disconnect()
 		print ('### Disconnecting to the device ' + adress_ip + ' is OK #### \n')
+
 		### fermeture du fichier de settings
 		device_lecture.close()
+		input("Tapez sur ENTREE pour continuer...")
 
-	elif ligne_choice == 'T' or ligne_choice == 't':
-		print("tous")
-
+	elif ligne_choice == 0 :
 		for device in device_lecture.readlines():
 			infos = device.split('_')
 			hostname = infos[0] 		#récupération de l'adress IP
@@ -154,6 +173,7 @@ def sauvegarde_devices(user_choice, device_type):
 			}
 
 			print ('\n#### Connecting to the device ' + adress_ip + ' #### \n')
+
 			### test de la connexion SSH
 			try:
 				net_connect = ConnectHandler(**devices)
@@ -188,12 +208,16 @@ def sauvegarde_devices(user_choice, device_type):
 			### Déconnexion
 			print ('### Disconnecting to the device ' + adress_ip + ' is OK #### \n')
 		net_connect.disconnect()
+		input("Tapez sur ENTREE pour contunuer.....")
 		
 	else :
 		print ("Erreur de saisie")
 	
 	### fermeture du fichier de settings
 	device_lecture.close()
+
+#############################################################################################
+### Menu Option
 
 option = "0"
 while option != "4":
@@ -220,5 +244,10 @@ while option != "4":
 			if option_cloud == "1" :
 				device_type = 'switch'
 				print(device_type)
-				time.sleep(10)
-
+				input("Tapez sur ENTREE pour contunuer.....")
+			elif option_cloud == "2" :
+				device_type = 'routeur'
+				print(device_type)
+				input("Tapez sur ENTREE pour contunuer.....")
+			
+	
